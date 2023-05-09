a3df28de20b17e6cf763fa3ac5bb98454a887aff
Joan <prosandcons500@gmail.com>
2023-05-09 13:33:47 +0300

    initialisation

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+// export * from 'components/FeedbackOptions/FeedbackOptions';
+export * from 'components/Section/Section';
+export * from 'components/Statistics/Statistics';
+export * from 'components/Notification/Notification';
